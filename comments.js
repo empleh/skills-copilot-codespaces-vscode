4579d2a6bff93c149ef6828e450c3f21cbb9c123
Christian Peters <christian.peters@gmail.com>
2023-06-26 12:49:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// create we server
+
+// import express
+const express = require('express');
+const router = express.Router();
+
+// import controller
+const commentController = require('../controllers/comment_controller');
+
+// import passport
+const passport = require('passport');
+
+// create route for create comment
+router.post('/create', passport.checkAuthentication, commentController.create);
+
+// create route for delete comment
+router.get('/destroy/:id', passport.checkAuthentication, commentController.destroy);
+
+// export router
+module.exports = router;
